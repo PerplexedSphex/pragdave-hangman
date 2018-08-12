26eb872e16093d2d6cbadc487932adf5444fbc9d
Sam Jacobs <sam.x.jacobs@gmail.com>
2018-08-12 14:00:43 -0700

    implement hangman game engine

--- hangman/lib/hangman.ex
+++ hangman/lib/hangman.ex
@@ -2,6 +2,15 @@ defmodule Hangman do
 
   alias Hangman.Game
 
-  defdelegate new_game(), to: Game
+  defdelegate new_game(),             to: Game
+  defdelegate tally(game),            to: Game
+
+  # put the monad at the api level
+  # tally returns %{ game_state:, turns_left:, letters: }
+  
+  def make_move(game, guess) do
+    game = Game.make_move(game, guess)
+    { game, tally(game) }
+  end
 
 end
--- hangman/test/hangman_test.exs
+++ hangman/test/hangman_test.exs
@@ -9,4 +9,41 @@ defmodule HangmanTest do
     assert game.turns_left == 7
     assert game.game_state == :initializing
   end
+
+  test "state isn't changed for :won or :lost game" do
+    for state <- [:won, :lost] do
+      game = Game.new_game()
+      |> Map.put(:game_state, :won)
+      assert ^game = Game.make_move(game, 'x')
+    end
+  end
+
+  test "first occurence of a letter is not already used" do
+    game = Game.new_game()
+    game = Game.make_move(game, 'x')
+    assert game.game_state != :already_used
+  end
+
+  test "second occurence of a letter is already used" do
+    game = Game.new_game()
+    game = Game.make_move(game, "x")
+    game = Game.make_move(game, "x")
+    assert game.game_state == :already_used
+  end
+
+  test "a good guess is recognized" do
+    game = Game.new_game("wibble")
+    game = Game.make_move(game, "w")
+    assert game.game_state == :good_guess
+    assert game.turns_left == 7
+  end
+
+  test "a bad guess is recognized" do
+    game = Game.new_game("wibble")
+    game = Game.make_move(game, "x")
+    assert game.game_state == :bad_guess
+    assert game.turns_left == 6
+  end
+  
+
 end
