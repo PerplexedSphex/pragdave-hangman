28a4ae4ab98e88805cb3d71bd334184e24c4d62f
Sam Jacobs <sam.x.jacobs@gmail.com>
2018-08-11 18:27:04 -0700

    implement dictionary random word

--- dictionary/lib/dictionary.ex
+++ dictionary/lib/dictionary.ex
@@ -1,18 +1,15 @@
 defmodule Dictionary do
-  @moduledoc """
-  Documentation for Dictionary.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Dictionary.hello
-      :world
-
-  """
-  def hello do
-    :world
+  
+  def random_word() do
+    word_list()
+    |> Enum.random()
   end
+
+  def word_list() do
+    "../assets/words.txt"
+    |> Path.expand(__DIR__)
+    |> File.read!()
+    |> String.split(~r/\n/)
+  end
+
 end
