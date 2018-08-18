7c3ffc8c3dc910b7a45549d077fac5e72c976609
Sam Jacobs <sam.x.jacobs@gmail.com>
2018-08-18 16:22:25 -0700

    delegate make_move in the api, fix tests to reflect

--- hangman/lib/hangman.ex
+++ hangman/lib/hangman.ex
@@ -4,13 +4,6 @@ defmodule Hangman do
 
   defdelegate new_game(),             to: Game
   defdelegate tally(game),            to: Game
-
-  # put the monad at the api level
-  # tally returns %{ game_state:, turns_left:, letters: }
-  
-  def make_move(game, guess) do
-    game = Game.make_move(game, guess)
-    { game, tally(game) }
-  end
+  defdelegate make_move(game, guess), to: Game
 
 end
--- hangman/test/hangman_test.exs
+++ hangman/test/hangman_test.exs
@@ -13,34 +13,34 @@ defmodule HangmanTest do
   test "state isn't changed for :won or :lost game" do
     for state <- [:won, :lost] do
       game = Game.new_game()
-      |> Map.put(:game_state, :won)
-      assert ^game = Game.make_move(game, 'x')
+      |> Map.put(:game_state, state)
+      assert { ^game, _tally } = Game.make_move(game, 'x')
     end
   end
 
   test "first occurence of a letter is not already used" do
     game = Game.new_game()
-    game = Game.make_move(game, 'x')
+    { game, _tally } = Game.make_move(game, 'x')
     assert game.game_state != :already_used
   end
 
   test "second occurence of a letter is already used" do
     game = Game.new_game()
-    game = Game.make_move(game, "x")
-    game = Game.make_move(game, "x")
+    { game, _tally } = Game.make_move(game, "x")
+    { game, _tally } = Game.make_move(game, "x")
     assert game.game_state == :already_used
   end
 
   test "a good guess is recognized" do
     game = Game.new_game("wibble")
-    game = Game.make_move(game, "w")
+    { game, _tally } = Game.make_move(game, "w")
     assert game.game_state == :good_guess
     assert game.turns_left == 7
   end
 
   test "a bad guess is recognized" do
     game = Game.new_game("wibble")
-    game = Game.make_move(game, "x")
+    { game, _tally } = Game.make_move(game, "x")
     assert game.game_state == :bad_guess
     assert game.turns_left == 6
   end
