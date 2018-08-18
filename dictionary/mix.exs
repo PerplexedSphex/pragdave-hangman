baa0a1dc6225a3566833f4a48ed0aaed137f74c1
Sam Jacobs <sam.x.jacobs@gmail.com>
2018-08-18 16:03:08 -0700

    agentify dictionary app

--- dictionary/mix.exs
+++ dictionary/mix.exs
@@ -14,6 +14,8 @@ defmodule Dictionary.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
+      # define entry point using mod
+      mod: { Dictionary.Application, [] },
       extra_applications: [:logger]
     ]
   end
